788d1edc1506a327f05f453bd47e96dc10480e9e
Deepa1996 <42736795+Deepa1996@users.noreply.github.com>
2018-08-27 19:17:03 +0530

    Add files via upload

---
+++ pgm40.py
@@ -0,0 +1,8 @@
+my_string = """Strings are gameon amongst gameon the most popular data types in Python. We can create the strings by enclosing characters briton in quotes. Python treats briton single quotes the same as double quotes."""
+def count_words(string):
+    for word in string.split():
+        if word.startswith("m") == True:
+            print(word,":",string.count(word))
+            string = string.replace(word,'')
+count_words(my_string)
+
---
+++ pgm9.py
@@ -0,0 +1,8 @@
+'''create dictionary of students with reg no and name'''
+print(thisdict)
+print("list after addition of an student")
+thisdict["chandana"]="181040012"
+print(thisdict)
+print("list after deletion of an element")
+del (thisdict["meghana"])
+print(thisdict)
